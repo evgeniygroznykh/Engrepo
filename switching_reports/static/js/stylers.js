9708b4f0a25944f4697c37cc98043d975dd0329a
Грозных Евгений <e_groznykh@mail.ru>
2020-09-18 16:16:46 +0300

    Changed the logic of styling on viewing switching reports
    If there are no switching reports for this work type, work type <div> changes it's background color to indicate, that it's empty

--- switching_reports/static/js/stylers.js
+++ switching_reports/static/js/stylers.js
@@ -2,3 +2,21 @@ function styleReportDescriptions (){
     $("div.report-desc-block:contains('Без замечаний')").css("background", "linear-gradient(to bottom, #339933 0%, #99ff33 100%)");
     $("div.report-desc-block:not(:contains('Без замечаний'))").css("background", "linear-gradient(to bottom, #ff0000 0%, #ff99cc 100%)");
 }
+
+function styleWorkTypeDivs (){
+    if(document.getElementById("Коммутация VAR").children.length <= 1){
+        document.getElementById("Коммутация VAR").style.background = 'grey';
+    }
+
+    if(document.getElementById("Коммутация Матч Премьер").children.length <= 1){
+        document.getElementById("Коммутация Матч Премьер").style.background = 'grey';
+    }
+
+    if(document.getElementById("Запись трансляции").children.length <= 1){
+        document.getElementById("Запись трансляции").style.background = 'grey';
+    }
+
+    if(document.getElementById("Коммутация Останкино").children.length <= 1){
+        document.getElementById("Коммутация Останкино").style.background = 'grey';
+    }
+}
