43628d37462b7af2b578abdef7c86ffa75f1277e
Грозных Евгений <e_groznykh@mail.ru>
2020-09-22 15:17:50 +0300

    .gitignore & config model changes
    Changed .gitignore file to ignore application of log files, added try/except block in config.py to handle situation, where config.json file is not found

--- cfg/config.py
+++ cfg/config.py
@@ -1,10 +1,16 @@
 import json
+from datetime import datetime as dt
 
 
 def get_json_config():
-    with open('cfg/config.json', 'r', encoding='utf-8') as read_json_config:
-        config_json = json.load(read_json_config)
-    return config_json
+    try:
+        with open('cfg/config.json', 'r', encoding='utf-8') as read_json_config:
+            config_json = json.load(read_json_config)
+        return config_json
+    except FileNotFoundError as exc:
+        with open('logs/app_log.txt', 'a+', encoding='utf-8') as log_file:
+            print(f"{dt.now()} | Config file was not found. | {exc.strerror} => {exc.filename}", file=log_file)
+        raise
 
 #GLOBALS
 JSON_CONFIG = get_json_config()
