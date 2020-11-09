e60c13964c08ac871c464c07080e12dfdaeb08ec
evgeniygroznykh <e_groznykh@mail.ru>
2020-11-09 12:18:12 +0300

    Changed formatting of cells

--- switching_reports/models/dataframe_handler.py
+++ switching_reports/models/dataframe_handler.py
@@ -3,55 +3,67 @@ from pandas import DataFrame
 
 
 #GLOBALS
+MERGE_COLUMNS = ['Дата создания', 'Заказчик', 'Исполнители']
 COLUMN_NAMES_TO_FORMAT = ['Дата редактирования', 'Начало трансляции', 'Окончание трансляции']
 CREATION_DATE_COLUMN_NAME = 'Дата создания'
 
 def _formatDateToDayOnly(dataframe, col_name):
     dataframe[col_name] = pandas.to_datetime(dataframe[col_name]).dt.date
 
+
 def _formatDateTo24H(dataframe, col_name):
     dataframe[col_name] = pandas.to_datetime(dataframe[col_name]).dt.strftime('%Y-%m-%d %H:%M')
 
-def getMergeIndexesWithSameValue(df:DataFrame, col_name, index_boundaries=None):
-    merge_indexes = []
-    if index_boundaries:
-        left_bound, right_bound = index_boundaries
-        df[col_name][left_bound:right_bound:].sort_values()
 
-        start_index = left_bound
-        end_index = start_index
-        for row in range(left_bound-1, right_bound):
-            if row == right_bound-1:
-                merge_indexes.append((start_index, end_index, df.loc[row, col_name]))
+def _sortDataframe(dataframe):
+    dataframe.sort_values(by=MERGE_COLUMNS, ascending=[True, True, True],
+                              inplace=True, ignore_index=True)
+
+
+def getMergeBoundaryIndexes(dataframe, col_name='Дата создания'):
+    merge_bound_indexes = []
+    start_index = 1
+    end_index = 1
+    for row in range(0, len(dataframe[col_name])):
+        if row == len(dataframe[col_name])-1:
+            merge_bound_indexes.append((start_index, end_index, dataframe.loc[row, col_name]))
+        else:
+            if dataframe.loc[row, col_name] == dataframe.loc[row+1, col_name]:
+                end_index += 1
             else:
-                if df.loc[row, col_name] == df.loc[row+1, col_name]:
-                    end_index += 1
-                else:
-                    merge_indexes.append((start_index, end_index, df.loc[row, col_name]))
-                    start_index = end_index + 1
-                    end_index = start_index
-    else:
-        start_index = 1
-        end_index = 1
-        for row in range(0, len(df[col_name])):
-            if row == len(df[col_name])-1:
-                merge_indexes.append((start_index, end_index, df.loc[row, col_name]))
+                merge_bound_indexes.append((start_index, end_index, dataframe.loc[row, col_name]))
+                start_index = end_index + 1
+                end_index = start_index
+    return merge_bound_indexes
+
+
+def getLocalMergeIndexes(boundary_indexes, col_name, dataframe):
+    local_merge_indexes = []
+    left_bound, right_bound = boundary_indexes[0], boundary_indexes[1]
+
+    start_index = left_bound
+    end_index = start_index
+    for row in range(left_bound-1, right_bound):
+        if row == right_bound-1:
+            local_merge_indexes.append((start_index, end_index, dataframe.loc[row, col_name]))
+        else:
+            if dataframe.loc[row, col_name] == dataframe.loc[row+1, col_name]:
+                end_index += 1
             else:
-                if df.loc[row, col_name] == df.loc[row+1, col_name]:
-                    end_index += 1
-                else:
-                    merge_indexes.append((start_index, end_index, df.loc[row, col_name]))
-                    start_index = end_index + 1
-                    end_index = start_index
-    return merge_indexes
+                local_merge_indexes.append((start_index, end_index, dataframe.loc[row, col_name]))
+                start_index = end_index + 1
+                end_index = start_index
+    return {col_name:local_merge_indexes}
+
 
 def getDataframeFromSwitchingReports(sw_reports:list):
     return pandas.DataFrame.from_records([sw_report.to_dict() for sw_report in sw_reports])
 
+
 def formatDataframeForXlsxUpload(raw_dataframe:DataFrame):
     _formatDateToDayOnly(raw_dataframe, CREATION_DATE_COLUMN_NAME)
-    _formatDateTo24H(raw_dataframe, 'Дата редактирования')
-    _formatDateTo24H(raw_dataframe, 'Начало трансляции')
-    _formatDateTo24H(raw_dataframe, 'Окончание трансляции')
+    for col_name in COLUMN_NAMES_TO_FORMAT:
+        _formatDateTo24H(raw_dataframe, col_name)
+    _sortDataframe(raw_dataframe)
     formatted_dataframe = raw_dataframe
     return formatted_dataframe
--- switching_reports/models/xlsx_handler.py
+++ switching_reports/models/xlsx_handler.py
@@ -1,78 +1,92 @@
 import pandas
 from pandas import DataFrame
 from datetime import datetime
-from switching_reports.models.dataframe_handler import getMergeIndexesWithSameValue
 from io import BytesIO
+from switching_reports.models.dataframe_handler import getMergeBoundaryIndexes, getLocalMergeIndexes
 
 
 #GLOBALS
-XLSX_CELL_FORMATS = {'date_format': {'align':'center', 'valign': 'vcenter', 'border': 1, 'num_format':'dd-mm-yyyy'},
+XLSX_CELL_FORMATS = {'short_date_time_format': {'align':'center', 'valign': 'vcenter', 'border': 1, 'num_format':'yyyy-mm-dd'},
+                    'full_date_time_format': {'align':'center', 'valign': 'vcenter', 'border': 1, 'num_format':'yyyy-mm-dd HH-MM'},
                      'text_format': {'align':'center', 'valign': 'vcenter', 'border': 1},
-                     'default_cell_format':{'align': 'center', 'valign': 'vcenter', 'border': 1}}
+                     'default_cell_format':{'align': 'center', 'valign': 'vcenter'}}
 SHEET_NAME = 'Switching_reports'
 XLSX_ENGINE = 'xlsxwriter'
-MERGE_COL_NUMBERS = {'Дата создания': 0, 'Заказчик': 8, 'Исполнители': 9}
+MERGE_COLUMNS = ['Дата создания', 'Заказчик', 'Исполнители']
+MERGE_COL_NUMBERS = {'Дата создания': 0, 'Исполнители': 9, 'Заказчик': 8}
+SHORT_DATETIME_COLUMNS = ['Дата создания']
+FULL_DATETIME_COLUMNS = ['Дата редактирования', 'Начало трансляции', 'Окончание трансляции']
+COLOR_FLAG = True
 
 
-def _addDateFormatToWorksheet(workbook, format_name='date_format'):
+def _getCellFormat(col_name, workbook):
+    if col_name in SHORT_DATETIME_COLUMNS:
+        format_name = 'short_date_time_format'
+        XLSX_CELL_FORMATS[format_name]['bg_color'] = 'gray' if COLOR_FLAG else 'white'
+    elif col_name in FULL_DATETIME_COLUMNS:
+        format_name = 'full_date_time_format'
+        XLSX_CELL_FORMATS[format_name]['bg_color'] = 'gray' if COLOR_FLAG else 'white'
+    else:
+        format_name = 'text_format'
+        XLSX_CELL_FORMATS[format_name]['bg_color'] = 'gray' if COLOR_FLAG else 'white'
     return workbook.add_format(XLSX_CELL_FORMATS[format_name])
 
-def _addTextFormatToWorksheet(workbook, format_name='text_format'):
-    return workbook.add_format(XLSX_CELL_FORMATS[format_name])
 
-def _mergeSimilarColumns(worksheet, merge_indexes, col_name, format):
-    for indexes_tuple in merge_indexes:
-        start_index, end_index, merge_data = indexes_tuple
-        worksheet.merge_range(start_index, MERGE_COL_NUMBERS[col_name], end_index, MERGE_COL_NUMBERS[col_name], merge_data, format)
-
-def _getXlsxFormats(workbook):
-    return (_addDateFormatToWorksheet(workbook), _addTextFormatToWorksheet(workbook))
-
-def _formatExcelData(writer:pandas.ExcelWriter, workbook):
+def _formatColumns(writer):
     worksheet = writer.sheets[SHEET_NAME]
+    workbook = writer.book
 
     default_format = workbook.add_format(XLSX_CELL_FORMATS['default_cell_format'])
     for col_excel_name in [chr(x) for x in range(65, 75)]:
+        print(f'Setting format for column {col_excel_name}:{col_excel_name}')
         worksheet.set_column(f'{col_excel_name}:{col_excel_name}', 50, cell_format=default_format)
 
-def _mergeCellsWithSameDate(dataframe:DataFrame, workbook, worksheet):
-    date_format = _getXlsxFormats(workbook)[0]
 
-    date_merge_indexes = getMergeIndexesWithSameValue(dataframe, list(MERGE_COL_NUMBERS.keys())[0])
-    _mergeSimilarColumns(worksheet, date_merge_indexes, list(MERGE_COL_NUMBERS.keys())[0], date_format)
+def _fillRows(writer, date_boundary_indexes):
+    global COLOR_FLAG
+    workbook = writer.book
+    worksheet = writer.sheets[SHEET_NAME]
 
-def _mergeCellsWithSameValues(dataframe:DataFrame, workbook, worksheet):
-    text_format = _getXlsxFormats(workbook)[1]
-    date_merge_indexes = getMergeIndexesWithSameValue(dataframe, list(MERGE_COL_NUMBERS.keys())[0])
+    bg_color = 'gray' if COLOR_FLAG else 'white'
+    lower_bound, upper_bound = date_boundary_indexes[0], date_boundary_indexes[1]
 
-    for date_merge_index_tuple in date_merge_indexes:
-        merge_boundaries = date_merge_index_tuple[0], date_merge_index_tuple[1]
-        for col_name in MERGE_COL_NUMBERS.keys():
-            if not col_name == 'Дата создания':
-                left_bound, right_bound = merge_boundaries
-                dataframe[col_name][left_bound:right_bound:].sort_values()
-                _mergeSimilarColumns(worksheet, getMergeIndexesWithSameValue(dataframe, col_name, merge_boundaries), col_name, text_format)
+    bg_color_cell_format = workbook.add_format({'align':'center', 'valign':'center', 'bg_color':bg_color, 'border':1})
+    for row in range(lower_bound, upper_bound+1):
+        worksheet.set_row(row, None, bg_color_cell_format)
+    COLOR_FLAG = False if COLOR_FLAG else True
+
+
+def _mergeRows(local_merge_indexes, col_name, writer):
+    column_number = MERGE_COL_NUMBERS[col_name]
+    for cur_merge_data in local_merge_indexes[col_name]:
+        start_merge_idx, end_merge_idx, merge_val = cur_merge_data
+        workbook = writer.book
+        worksheet = writer.sheets[SHEET_NAME]
+
+        merge_format = _getCellFormat(col_name, workbook)
+        worksheet.merge_range(start_merge_idx, column_number, end_merge_idx, column_number, merge_val, merge_format)
+
 
 def getReadableFilenameFromDates(from_date:datetime, to_date:datetime):
     return f"{from_date.strftime('%Y-%m-%d')}_{to_date.strftime('%Y-%m-%d')}.xlsx"
 
+
 def writeDataframeToXlsx(dataframe:DataFrame):
+    merge_boundary_indexes_from_date = getMergeBoundaryIndexes(dataframe)
+
     output = BytesIO()
     writer = pandas.ExcelWriter(output, engine=XLSX_ENGINE)
 
     dataframe.to_excel(writer, index=False, startrow=0, sheet_name=SHEET_NAME)
-    worksheet = writer.sheets[SHEET_NAME]
-    workbook = writer.book
+    _formatColumns(writer)
 
-    _formatExcelData(writer, workbook)
-    _mergeCellsWithSameDate(dataframe, workbook, worksheet)
-    _mergeCellsWithSameValues(dataframe, workbook, worksheet)
+    for cur_merge_boundaries in merge_boundary_indexes_from_date:
+        for col in MERGE_COLUMNS:
+            col_local_merge_indexes = getLocalMergeIndexes(cur_merge_boundaries, col, dataframe)
+            _mergeRows(col_local_merge_indexes, col, writer)
+        _fillRows(writer, cur_merge_boundaries)
 
     writer.close()
     output.seek(0)
 
     return output
-
-
-
-
